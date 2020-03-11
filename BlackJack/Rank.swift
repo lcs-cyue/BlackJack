6696ab84930c72e79cfe470fe57858636cc08aa4
Chelsea Yue <cyue@lcs.on.ca>
2020-03-11 19:57:15 -0400

    part of #8 (changed some values and set up the rank)

--- BlackJack/Rank.swift
+++ BlackJack/Rank.swift
@@ -18,37 +18,37 @@ enum Rank : Int {
     // List possible cases
     case ace = 1, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
     
+    
     // Computed property to return rank
     // Really just a convenience property to make code more readable, it returns the raw value of the enumeration case but using the property name of "rank"
     var rank : Int {
         switch self {
-            default:
-                return self.rawValue
+        default:
+            return self.rawValue
         }
+        
     }
 
-
-    
-//    // Does one rank (this instance) beat another rank?
-//    func beats(_ otherRank: Rank) -> Bool {
-//        return self.rank > otherRank.rank
-//    }
+    //    // Does one rank (this instance) beat another rank?
+    //    func beats(_ otherRank: Rank) -> Bool {
+    //        return self.rank > otherRank.rank
+    //    }
     
     
     // Report the simple description of this rank
     func simpleDescription() -> String {
-       switch self {
-       case .ace:
-         return "Ace"
-       case .jack:
-         return "Jack"
-       case .queen:
-         return "Queen"
-       case .king:
-         return "King"
-       default:
-         return String(self.rawValue)
-       }
-     }
+        switch self {
+        case .ace:
+            return "Ace"
+        case .jack:
+            return "Jack"
+        case .queen:
+            return "Queen"
+        case .king:
+            return "King"
+        default:
+            return String(self.rawValue)
+        }
+    }
     
 }
