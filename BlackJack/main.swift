684aca4cad67ff8594d8a96d15c9372019e60510
Chelsea Yue <cyue@lcs.on.ca>
2020-04-11 02:17:47 -0400

    "Added some comments and potential problems"

--- BlackJack/main.swift
+++ BlackJack/main.swift
@@ -7,10 +7,22 @@
 //
 
 import Foundation
+//some problems: 1.interactive mode, 2.order of the deck 3...
 
-
+// Create a new datatype to represent a game of BlackJack
 class BlackJack {
     
+    // The deck of cards
+    var deck : Deck
+    
+    // The hands for each player
+    var player1 : Hand
+    var player2 : Hand
+    var offence : Hand
+    var defence : Hand
+    
+    // The pile
+    var pile : Hand
     
     
     init() {
@@ -33,25 +45,41 @@ class BlackJack {
         print("START GAME")
         print("==========\n")
         
+        print("It's Player1's turn to draw")
+        
         play()
+        
+        
+        //Interactive mode???????IDK how to continue
+        
+        //create a dictionary that uses the first letter as a key and the words that begin with that letter as the values
+        let dict = [
+            "y": ["Yes"],
+            "n": ["No"]
+        ]
+        
+        
         
     }
     
-    // The deck of cards
-    var deck : Deck
-    
-    // The hands for each player
-    var player1 : Hand
-    var player2 : Hand
-    var offence : Hand
-    var defence : Hand
-    
-    // The pile
-    var pile : Hand
     
     
     private func play() {
         keyPress()
+        
+        //player1 starts drawing the top card by pressing "Y" and ends drawing by pressing "N"
+        
+        //display a warning message if the value surpasses 17
+        
+        //player2 starts drawing the top card by pressing "Y" and ends drawing by pressing "N"
+        
+        //display a warning message if the value surpasses 17
+        
+        //compare the final value of player1&2
+        
+        //determine the winner for the round, display score
+        
+        //repeat until one player wins 10 rounds
         
     }
     
@@ -59,6 +87,7 @@ class BlackJack {
         print("Press 'Y' to continue drawing", terminator: "y")
         readLine()
     }
+    
     
 }
 
